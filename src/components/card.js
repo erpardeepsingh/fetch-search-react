9ca5b37f75a935ec487729a8beaa1e0bd811f793
--global <--global>
2020-12-13 15:02:37 +0530

    initial

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,10 @@
 import App from "../App"
 
 function Card(props){
-return (<div className="card">
+return (<a href={props.data.url} target="_blank" className="card">
     <div className="card-image" style={{backgroundImage: `url(${props.data.image_url})`}}/>
     <div className="title">{props.data.title}</div>
-</div>)
+</a>)
 }
 
 export default Card;
